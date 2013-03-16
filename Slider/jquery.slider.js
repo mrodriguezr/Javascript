36ccf1311f8883f8709e9a6ad607c0af52f09cc3
Melvin Rodriguez <mrodriguezr66@gmail.com>
2013-03-16 17:33:34 -0600

    Migrate laptop

--- Slider/jquery.slider.js
+++ Slider/jquery.slider.js
@@ -1,29 +1,55 @@
 
 (function($){
-	var defaults = {
-		speed: 5000,
+	var settings = {
+		animation: 'fadeout',
+		animationSpeed: 2000,
+		timeout: 4000,
 		text: '',
-		textPosition: bottom,
-		color: 'blue'
+		textPosition: 'bottom',
+		cssClass: 'rslides'
 	};
-	
+	var rotate,
+		index = 0,
+        visible = {"float": "left", "position": "relative", "zIndex": 2},
+        hidden = {"float": "none", "position": "absolute", "zIndex": 1};
+		
 	var methods = {
-		init : function(message, options){
-			var _self = this;
-			$.extend(defaults, defaults, options);
+		init : function(options){
+			var _self = $(this);
+			$.extend(settings, settings, options);
 			
 			return _self.each(function(){
-				methods._startSlide(this,message);
+				methods._startSlide(_self, this);
 			});
 		},
-		_startSlide: function(elem, message){
-			$(elem).text(message);
-			$(elem).css('color', defaults.color);
+		_startSlide: function(context, elem){
+			var $slide = context.children(),
+				length = $slide.size();
+			
+			rotate = setInterval(function () {
+            $slide.stop(true, true);
+
+            var idx = index + 1 < length ? index + 1 : 0;
+            methods._slideItem(context, idx);
+            }, settings.timeout);
+          },
+        _slideItem: function (elem, idx) {
+			var $slide = elem.children();
+			$slide
+			  .stop()
+			  .fadeOut(settings.animationSpeed, function () {
+				$(this).css(hidden);
+			  })
+			  .eq(idx)
+			  .fadeIn(settings.animationSpeed, function () {
+				$(this).css(visible);
+				index = idx;
+				$("#test").text($slide.eq(idx).find('img').attr('alt'));
+			  });
 		}
 	};
 	
-	//Setting up the plugin
-	$.fn.slider = function(message, options){
+	$.fn.slider = function(options){
 		return methods.init.apply(this, arguments);
 	};
 	
